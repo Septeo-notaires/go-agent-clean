61d08ce589dd6ac41113b4af135a060329b9f4d1
Damien Venant-Valéry <damien.venantvalery@septeo.com>
2025-03-26 08:30:04 +0100

    feat: Mise en place de l'architecture

---
+++ internal/factory.go
@@ -0,0 +1,31 @@
+package internal
+
+type OsType int
+
+const (
+	Linux OsType = iota
+	Windows
+)
+
+type TerminalFactory struct {
+	osType          OsType
+	interpreterName string
+}
+
+func NewTerminalFactory(interpreterName string, osType OsType) TerminalFactory {
+	return TerminalFactory{
+		osType:          osType,
+		interpreterName: interpreterName,
+	}
+}
+
+func (t *TerminalFactory) NewTerminal() ITerminal {
+	switch t.osType {
+	case Windows:
+		return &WindowsTerminal{}
+	case Linux:
+		return &LinuxTerminal{}
+	default:
+		return nil
+	}
+}
